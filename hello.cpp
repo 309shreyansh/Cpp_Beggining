9c31e3caea3e492fd89e2b1f68e5e49954df83dd
Shreyansh Singh <sshreyansh336@gmail.com>
2024-09-05 09:19:03 +0530

    /* used for multiline comments

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 int main (){
-      std::cout  << "I like pizza!";
+      
+      std::cout  << "I like pizza!" << '\n';
+
+      std::cout  << "Its really good!";
       return 0;
 }
